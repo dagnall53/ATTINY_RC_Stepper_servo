887e69912ac7133243de6c463bb4718d26621cec
dagnall <dagnall53@googlemail.com>
2021-02-06 12:45:14 +0000

    Update ATTINY_RC_Stepper_servo.ino
    Updated with Half step and corrected drive code

--- ATTINY_RC_Stepper_servo/ATTINY_RC_Stepper_servo.ino
+++ ATTINY_RC_Stepper_servo/ATTINY_RC_Stepper_servo.ino
@@ -7,6 +7,15 @@
  */ 
 
  /*
+  /*
+ * MODIFIED from  * ATTINY85_RC_Receiver.c
+ *
+ * Created: 01.01.2019 20:06:29
+ * Author : Andreas
+ * Description: Read and interpret signals from RC Receivers
+ */ 
+
+ /*
   * Dagnall 2021 modified to use alternate Timer
   * Board set to 8Mhz no usb
   * 
@@ -49,32 +58,21 @@ bool PositionAchieved;
 
 #define HALF_STEP true
 
-#define StepSpeed  500  //uS per step // seems to have limited range (400-1200?) of operation with the tiny motors.. (resonance?)  
 
-#define GAIN 20 //20 sets  200 deg for 100 count
+#define StepSpeed 300 //uS per step //  slower for non half step? 
+
+#define GAIN 15 //
 #define RC_RECEIVER_PORT PB0
 #define DEAD_ZONE GAIN*1.5
 
-#define OUT0 PB1
-#define OUT1 PB2
-#define OUT2 PB3
-#define OUT3 PB4
-
-#define OUT0_ON PORTB |= (1 << OUT0)
-#define OUT1_ON PORTB |= (1 << OUT1)
-#define OUT2_ON PORTB |= (1 << OUT2)
-#define OUT3_ON PORTB |= (1 << OUT3)
-#define OUT0_OFF PORTB &= ~(1 << OUT0)
-#define OUT1_OFF PORTB &= ~(1 << OUT1)
-#define OUT2_OFF PORTB &= ~(1 << OUT2)
-#define OUT3_OFF PORTB &= ~(1 << OUT3)
 
 
 
-
-
-
-
+// drive to motor drive ic
+#define OUTA PB1
+#define OUT_An PB3
+#define OUTB PB2
+#define OUT_Bn PB4
 
 #define RISING_EDGE PINB & (1 << RC_RECEIVER_PORT)
 
@@ -91,16 +89,16 @@ void Calibrate_OSCILLATOR(void){
     // frequency was measured with oscilloscope via PB4 (CKOUT fuse needs to be set for this)
     
     // OSCCAL needs to be calibrated per chip
-    OSCCAL = 130;//142;  //Dag needs adjustng to ensure that servo pulses are seen ok. too high and one end will "fold back" in response                          // this gave 7,96MHz (peaks of 8MHz) for value ranges see datasheet page 52
+    OSCCAL = 135;//142;  //Dag needs adjustng to ensure that servo pulses are seen ok. too high and one end will "fold back" in response                          // this gave 7,96MHz (peaks of 8MHz) for value ranges see datasheet page 52
 }
 
 void Init_PORT(void) {
     // this function initializes the pins which are used as output / inputs
 
-    DDRB |= (1 << OUT0);                 // set led port as output
-    DDRB |= (1 << OUT1);  
-    DDRB |= (1 << OUT2);  
-    DDRB |= (1 << OUT3);  
+    DDRB |= (1 << OUTA);                 // set led port as output
+    DDRB |= (1 << OUT_An);  
+    DDRB |= (1 << OUTB);  
+    DDRB |= (1 << OUT_Bn);  
   
     DDRB &= ~(1 << RC_RECEIVER_PORT);        // set receiver port as input
 }
@@ -135,36 +133,33 @@ ISR(PCINT0_vect){
     // interrupt service routine for pin change interrupt
     // if PINB is HIGH, a rising edge interrupt has happened
     // if PINB is LOW, a falling edge interrupt has happened
-    
-// no does not work.. if(PositionAchieved){  // dag test to see if this works only do when servo has achieved correct position
-  
+    // modified to use TCNT0 to allow delay to work.. 
     if ( RISING_EDGE ){       // check if rising edge pin change interrupt (beginning of servo pulse)
-   //  TCNT1 = 0;                           // reset counter (TCNT1 page 91 TCNT0 page 78
+   //   TCNT1 = 0;                           // reset counter (TCNT1 page 91 TCNT0 page 78
    //   TCCR1 = (1 << CS12) | (1 << CS11) | (1 << CS10);   // start timer1 with prescaler CK/64 --> 250 steps per 2ms (TCCR1 page 89
         TCNT0 = 0; 
         TCCR0B = (1 <<CS01) | (1<< CS00) ; // start timero ck/64 page 80
         return;
        }
-
     // only reached when falling edge detected (end of servo pulse)
     //count = TCNT1;                           // take timer value to local variable
     //TCCR1 = 0;                               // stop timer
-      count = TCNT0;                           // take timer value to local variable 125-254 
+      count = TCNT0;                           // take timer value to local variable ~ 125-254 if osc correct
       TCCR0B = 0;                               // stop timer
-    
       GIMSK &= ~(1 << PCIE);                   // Pin Change Interrupt Disable (datasheet page 51)
       pulse_ready=1;
-//no }     
 }
 
 
 
-void Stepper_Drive(int  in) {
-  //delay(2);
-  delayMicroseconds(StepSpeed);
+void Stepper_Drive(int  in) {    // simple way to select 4 pin or 5 pin steppers etc 
+    delayMicroseconds(StepSpeed);
 //  Stepper4_LOW_Drive(in);
-  Stepper4_Drive(in);
-//  if (HALF_STEP) {Stepper4_half_step_Drive(in);} else {Stepper4_Drive(in); } // simple way to select 4 pin or 5 pin steppers
+//  Stepper4_Drive(in);
+   // Stepper4_half_step_Drive(in);
+  if (HALF_STEP) {Stepper4_half_step_Drive(in);} else {Stepper4_Drive(in); } 
+ 
+    
   }
 
 void Stepper4_Drive ( int  in){
@@ -178,20 +173,16 @@ void Stepper4_Drive ( int  in){
  *  0  0  0  1
  */
    uint8_t internal;
-  internal=in % 3;    //(modulo keeps internal 0-3)
+  internal= in % 4;    //(modulo keeps internal 0-3)
    switch (internal){
-     case 0:{digitalWrite(OUT0, HIGH);digitalWrite(OUT1, LOW);digitalWrite(OUT2, LOW);digitalWrite(OUT3, LOW);
-         }
-    break;
-    case 1:{ digitalWrite(OUT0, LOW);digitalWrite(OUT1, LOW);digitalWrite(OUT2, HIGH);digitalWrite(OUT3, LOW);
-         }
-     break;
-  case 2:{   digitalWrite(OUT0, LOW);digitalWrite(OUT1, HIGH);digitalWrite(OUT2, LOW);digitalWrite(OUT3, LOW);
-         }
-     break;
-    case 3:{ digitalWrite(OUT0, LOW);digitalWrite(OUT1, LOW);digitalWrite(OUT2, LOW);digitalWrite(OUT3, HIGH);
-         }
-    break;
+     case 0: digitalWrite(OUTA, HIGH);digitalWrite(OUT_An, LOW);digitalWrite(OUTB, LOW);digitalWrite(OUT_Bn, LOW);
+            break;
+    case 1:   digitalWrite(OUTA, LOW);digitalWrite(OUT_An, LOW);digitalWrite(OUTB, HIGH);digitalWrite(OUT_Bn, LOW);
+            break;
+    case 2:   digitalWrite(OUTA, LOW);digitalWrite(OUT_An, HIGH);digitalWrite(OUTB, LOW);digitalWrite(OUT_Bn, LOW);
+            break;
+    case 3:   digitalWrite(OUTA, LOW);digitalWrite(OUT_An, LOW);digitalWrite(OUTB, LOW);digitalWrite(OUT_Bn, HIGH);
+            break;
         }
   }
 
@@ -200,145 +191,93 @@ void Stepper4_Drive ( int  in){
 
 void Stepper5_Drive(int  in) {
 
-switch (in % 7)  {  //(modulo keeps internal 0-7)) 
+switch (in % 8)  {  //(modulo keeps internal 0-7)) 
 case 0:
-digitalWrite(OUT0, LOW);digitalWrite(OUT1, LOW);digitalWrite(OUT2, LOW);digitalWrite(OUT3, HIGH);
+digitalWrite(OUTA, LOW);digitalWrite(OUT_An, LOW);digitalWrite(OUTB, LOW);digitalWrite(OUT_Bn, HIGH);
 break;
 case 1:
-digitalWrite(OUT0, LOW);digitalWrite(OUT1, LOW);digitalWrite(OUT2, HIGH);digitalWrite(OUT3, HIGH);
+digitalWrite(OUTA, LOW);digitalWrite(OUT_An, LOW);digitalWrite(OUTB, HIGH);digitalWrite(OUT_Bn, HIGH);
 break;
 case 2:
-digitalWrite(OUT0, LOW);digitalWrite(OUT1, LOW);digitalWrite(OUT2, HIGH);digitalWrite(OUT3, LOW);
+digitalWrite(OUTA, LOW);digitalWrite(OUT_An, LOW);digitalWrite(OUTB, HIGH);digitalWrite(OUT_Bn, LOW);
 break;
 case 3:
-digitalWrite(OUT0, LOW);digitalWrite(OUT1, HIGH);digitalWrite(OUT2, HIGH);digitalWrite(OUT3, LOW);
+digitalWrite(OUTA, LOW);digitalWrite(OUT_An, HIGH);digitalWrite(OUTB, HIGH);digitalWrite(OUT_Bn, LOW);
 break;
 case 4:
-digitalWrite(OUT0, LOW);digitalWrite(OUT1, HIGH);digitalWrite(OUT2, LOW);digitalWrite(OUT3, LOW);
+digitalWrite(OUTA, LOW);digitalWrite(OUT_An, HIGH);digitalWrite(OUTB, LOW);digitalWrite(OUT_Bn, LOW);
 break;
 case 5:
-digitalWrite(OUT0, HIGH);digitalWrite(OUT1, HIGH);digitalWrite(OUT2, LOW);digitalWrite(OUT3, LOW);
+digitalWrite(OUTA, HIGH);digitalWrite(OUT_An, HIGH);digitalWrite(OUTB, LOW);digitalWrite(OUT_Bn, LOW);
 break;
 case 6:
-digitalWrite(OUT0, HIGH);digitalWrite(OUT1, LOW);digitalWrite(OUT2, LOW);digitalWrite(OUT3, LOW);
+digitalWrite(OUTA, HIGH);digitalWrite(OUT_An, LOW);digitalWrite(OUTB, LOW);digitalWrite(OUT_Bn, LOW);
 break;
 case 7:
-digitalWrite(OUT0, HIGH);digitalWrite(OUT1, LOW);digitalWrite(OUT2, LOW);digitalWrite(OUT3, HIGH);
+digitalWrite(OUTA, HIGH);digitalWrite(OUT_An, LOW);digitalWrite(OUTB, LOW);digitalWrite(OUT_Bn, HIGH);
 break;
 default:
-digitalWrite(OUT0, LOW);digitalWrite(OUT1, LOW);digitalWrite(OUT2, LOW);digitalWrite(OUT3, LOW);
+digitalWrite(OUTA, LOW);digitalWrite(OUT_An, LOW);digitalWrite(OUTB, LOW);digitalWrite(OUT_Bn, LOW);
 break;
 }
 
 }
 
 void Stepper4_half_step_Drive(int  in) {
-  /*Min power consumption
-   * 
-Terminal 1a: + 0 - 0 
-Terminal 1b: - 0 + 0 
-Terminal 2a: 0 + 0 - 
-Terminal 2b: 0 - 0 + 
-time —>
 
-max torque
-Terminal 1a: + + - - 
-Terminal 1b: - - + + 
-Terminal 2a: - + + - 
-Terminal 2b: + - - + 
 
-   * 
-   * 
-   * 
-   */
-
-switch (in % 7)  {  //(modulo keeps internal 0-7)) 
+switch (in % 8)  {  //(modulo keeps internal 0-7)) 
 case 0:
-digitalWrite(OUT0, LOW);digitalWrite(OUT1, HIGH);digitalWrite(OUT2, LOW);digitalWrite(OUT3, HIGH);
+digitalWrite(OUTA, HIGH);digitalWrite(OUT_An, LOW);digitalWrite(OUTB, LOW);digitalWrite(OUT_Bn, LOW);
 break;
 case 1:
-digitalWrite(OUT0, LOW);digitalWrite(OUT1, HIGH);digitalWrite(OUT2, LOW);digitalWrite(OUT3, LOW);
+digitalWrite(OUTA, HIGH);digitalWrite(OUT_An, LOW);digitalWrite(OUTB, HIGH);digitalWrite(OUT_Bn, LOW);
 break;
 case 2:
-digitalWrite(OUT0, LOW);digitalWrite(OUT1, HIGH);digitalWrite(OUT2, LOW);digitalWrite(OUT3, HIGH);
+digitalWrite(OUTA, LOW);digitalWrite(OUT_An, LOW);digitalWrite(OUTB, HIGH);digitalWrite(OUT_Bn, LOW);
 break;
 case 3:
-digitalWrite(OUT0, LOW);digitalWrite(OUT1, LOW);digitalWrite(OUT2, LOW);digitalWrite(OUT3, HIGH);
+digitalWrite(OUTA, LOW);digitalWrite(OUT_An, HIGH);digitalWrite(OUTB, HIGH);digitalWrite(OUT_Bn, LOW);
 break;
 case 4:
-digitalWrite(OUT0, HIGH);digitalWrite(OUT1, LOW);digitalWrite(OUT2, LOW);digitalWrite(OUT3, HIGH);
+digitalWrite(OUTA, LOW);digitalWrite(OUT_An, HIGH);digitalWrite(OUTB, LOW);digitalWrite(OUT_Bn, LOW);
 break;
 case 5:
-digitalWrite(OUT0, HIGH);digitalWrite(OUT1, LOW);digitalWrite(OUT2, LOW);digitalWrite(OUT3, LOW);
+digitalWrite(OUTA, LOW);digitalWrite(OUT_An, HIGH);digitalWrite(OUTB, LOW);digitalWrite(OUT_Bn, HIGH);
 break;
 case 6:
-digitalWrite(OUT0, HIGH);digitalWrite(OUT1, LOW);digitalWrite(OUT2, LOW);digitalWrite(OUT3, HIGH);
+digitalWrite(OUTA, LOW);digitalWrite(OUT_An, LOW);digitalWrite(OUTB, LOW);digitalWrite(OUT_Bn, HIGH);
 break;
 case 7:
-digitalWrite(OUT0, LOW);digitalWrite(OUT1, LOW);digitalWrite(OUT2, LOW);digitalWrite(OUT3, HIGH);
+digitalWrite(OUTA, HIGH);digitalWrite(OUT_An, LOW);digitalWrite(OUTB, LOW);digitalWrite(OUT_Bn, HIGH);
 break;
 default:
-digitalWrite(OUT0, LOW);digitalWrite(OUT1, LOW);digitalWrite(OUT2, LOW);digitalWrite(OUT3, LOW);
+digitalWrite(OUTA, LOW);digitalWrite(OUT_An, LOW);digitalWrite(OUTB, LOW);digitalWrite(OUT_Bn, LOW);
 break;
 }
 
 }
 
 
-void Stepper4_LOW_Drive(int  in) {
-  /*
-   * max torque
-Terminal 1a: + + - - 
-Terminal 1b: - - + + 
-Terminal 2a: - + + - 
-Terminal 2b: + - - + 
 
-Min power consumption
-Terminal 1a: + 0 - 0 
-Terminal 1b: - 0 + 0 
-Terminal 2a: 0 + 0 - 
-Terminal 2b: 0 - 0 + 
-time —>
-   */
-
-switch (in % 3)  {  //(modulo keeps internal 0-7)) 
-case 0:
-digitalWrite(OUT0, LOW);digitalWrite(OUT1, HIGH);digitalWrite(OUT2, LOW);digitalWrite(OUT3, LOW);
-break;
-case 1:
-digitalWrite(OUT0, LOW);digitalWrite(OUT1, LOW);digitalWrite(OUT2, LOW);digitalWrite(OUT3, HIGH);
-break;
-case 2:
-digitalWrite(OUT0, HIGH);digitalWrite(OUT1, LOW);digitalWrite(OUT2, LOW);digitalWrite(OUT3, LOW);
-break;
-case 3:
-digitalWrite(OUT0, LOW);digitalWrite(OUT1, LOW);digitalWrite(OUT2, HIGH);digitalWrite(OUT3, LOW);
-break;
-
-default:
-digitalWrite(OUT0, LOW);digitalWrite(OUT1, LOW);digitalWrite(OUT2, LOW);digitalWrite(OUT3, LOW);
-break;
-}
-
-}
 
 void Achieved(){  
-  PositionAchieved=true;digitalWrite(OUT0, LOW);digitalWrite(OUT1, LOW);digitalWrite(OUT2, LOW);digitalWrite(OUT3, LOW);
-}
-
-
+  PositionAchieved=true;digitalWrite(OUTA, LOW);digitalWrite(OUT_An, LOW);digitalWrite(OUTB, LOW);digitalWrite(OUT_Bn, LOW);
+  }
 
 void Move_To (int pos){  
-  int diff,aim;
-  aim=(pos*GAIN); // gain here! 
+  int diff,aim,deadzone; bool dir;
+  deadzone=DEAD_ZONE;  // so we can use halfstep bool
+  aim=(pos*GAIN); // use gain here! 
+  if (HALF_STEP){aim=aim*2;deadzone=deadzone*2;}
+  
   diff = aim - Stepper_Position;  
-  bool dir;
-  if (abs(diff) >= (DEAD_ZONE)){
+  if (abs(diff) >= (deadzone)){
     oldpos=Stepper_Position;
     dir=(abs(diff)== diff);
-                 // should probably be while loop, but I had trouble with that and this works.. 
+                 // should probably be a while.. loop, but I had trouble with that and this works.. 
   if (dir) {for (int x=oldpos; x<=aim;x++){Stepper_Position=x;Stepper_Drive(Stepper_Position);}}
-       else{  for (int x=oldpos; x>=aim;x--){Stepper_Position=x;Stepper_Drive(Stepper_Position); }}
+       else{for (int x=oldpos; x>=aim;x--){Stepper_Position=x;Stepper_Drive(Stepper_Position); }}
       Achieved();           
      }
   }
@@ -361,6 +300,9 @@ void IOTEST(){
 void setup(){
     Calibrate_OSCILLATOR();
     Init_PORT();
+   // send out nominal 50kHz for calibration
+   //for (long x=0; x<=500000;x++){delayMicroseconds(1);digitalWrite(OUT_Bn, HIGH);delayMicroseconds(1);digitalWrite(OUT_Bn,LOW);}
+   //
     Init_INTERRUPTS();
     Stepper_Position=3200;
     demand=0;
