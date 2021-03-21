1111344b8ca821da79a132a881573e71c6b7fe7e
dagnall <dagnall53@googlemail.com>
2021-03-21 18:50:41 +0000

    Update ATTINY_RC_Stepper_Servo.ino
    Updated comments

--- ATTINY_RC_Stepper_servo/ATTINY_RC_Stepper_Servo/ATTINY_RC_Stepper_Servo.ino
+++ ATTINY_RC_Stepper_servo/ATTINY_RC_Stepper_Servo/ATTINY_RC_Stepper_Servo.ino
@@ -86,40 +86,49 @@ bool PositionAchieved,Analog_mode;
 
 //---------are we going to simulate an Analog servo, or a "End to end" device that switches at about PWM ==90 degrees? 
 
-//#define ANALOG  //de comment for analog else end to end 
+//#define ANALOG  //de comment for analog else unit drives end to end and uses approximatley mid point pwm as threshold (with hystresis)  
 
 
 #define FullRange 1500  // ~1500 for exact full range movement on the linear servo and also defines travel for non analog  NOTE: larger value keeps motor driving until it hits endstops
-#define StepSpeed 250 //uS per step 300 is about max speed at half step //  perhaps slower needed for full step? 
-#define HALF_STEP true  //false at 350us gives about 8 oz thrust, BUT this is close to the dynamic max holding thrust;
-                         // STATIC holding thrust is about 12-16 oz, but once initial movement is started it drops to about 8/9 oz needed 
-                         //to keep the slider moving. 
-                        // half step drives at HALF speed!
+#define StepSpeed 500 //uS per step 300 is about max speed at half step //  perhaps slower needed for full step? 
+#define HALF_STEP true  //Full stepping (Halfstep false) at 350us gives about 8 oz thrust, BUT this is close to the dynamic max holding thrust;
+                         // HAlf stepping gives slightly higher thrust and higher current drain..
+                         // About 10 Oz seen, STATIC holding thrust is about 12-16 oz, 
+                         // but once motor gets to max thrust, any "slip" can slip it back to about 8/9 oz before it holds again. 
+                         
 
-// GAIN is set to give full range for "0-125" (nominal 0-1.25ms)range (== 1.0 to 2.25ms nominal response from the RC pulse as measured by the interrupt timer.
-// so if the Range measurement is 36 counts (of 40 steps), the full range steps are 1440
-// so gain sould be 1440/125 = (integer!) 12 
-#define GAIN 12 
+#define GAIN 15
+                      // GAIN is set to give full range for "0-100" (nominal 0-1ms)range (== 1.0 to 2.ms nominal response from the RC pulse as measured by the interrupt timer.
+                      // so if the Range measurement is 36 counts (of 40 steps), the full range steps are 1440, say 1500?
+                      // so gain should be 1500/100 = (integer!) 15 
+
 
 void Calibrate_OSCILLATOR(void){// OSCCAL needs to be calibrated per chip
-    // un comment #define Send_10K_CAL to send out 10kHz on PB3
-    // Then use Freq counter on PB3 to measure and adjust OSCCAL accordingly
-    // re-comment  "Send_10K_CAL" before final programing 
-// revised comments, because the interrupt register only gets to 2ms for 256, its a good idea to slow the whole clock down, to avoid "wrap around"
-// My Servo tester goes to 2.2ms , so we would need the clock to slow about 10% (min) to prevent wrap around. 
-// 63 is apparently about 75% speed (see datasheet) 
-// this would give timing at 10.6us steps in the interrupt allowing measuring to about 2.7ms. 
-#define OSCNOM 88    
-    OSCCAL = 97;// sets to about 88% nominal clock..;  //needs adjustng (slowing down) to ensure that servo pulses are seen ok. too high and one end will "fold back" in response to >2ms pulses      
+   //On chip oscillator Ideally needs adjustng (slowing down) to ensure that servo pulses are seen ok. too high and one end will "fold back" in response to >2ms pulses
+   // because the interrupt register only gets to 2ms for 256 count, its a good idea to slow the whole clock down, to avoid "wrap around"
+   // I have attempted to catch this in the count validation, but not entirely sucessfully. 
+   // A better approach is to simply to slow the oscillator down deliberately. 
+   // My Servo tester goes to 2.2ms , so we would need the clock to slow about 10% (min) to prevent wrap around. 
+        
    //  for value ranges see datasheet page 31  note value ranges overlap at 127/128 ! so you may not get precisely the changes anticipated in this region.
    // Clock speed is approximately (50+(0.397*OSCAL))% of the nominal 8Mhz.
+
+
+// EXAMPLE 63 is apparently about 75% speed (see datasheet) 
+// this would give timing at 10.6us steps in the interrupt allowing measuring to about 2.7ms.  
+  
+    OSCCAL = 90;// sets to about 88% nominal clock..;  
+
+    //ALSO note, I have a #define Send_10K_CAL to send out 10kHz on PB3
+    // Then I can use a Freq counter on PB3 to check, measure and adjust OSCCAL if needed
+    // BUT REMEMBER to re-comment  "Send_10K_CAL" before final programing 
    }
 
 
-//PORTS 
+//PORTS define 
 #define RC_RECEIVER_PORT PB0
 // drive to motor drive ic
-//pinout for jlpcb board 
+//pinout for jlpcb board and V2 PCBWAY board
 #define OUTA PB1
 #define OUT_An PB2
 #define OUTB PB3
@@ -139,35 +148,36 @@ void Init_PORT(void) {
 
 
 void OUT_Control(float position,uint8_t port){
-    // this function switches port on depending on RC signal
+    // this Andreas function switches port on depending on RC signal
     // position variable indicates 0%-100% stick position
-    // ---------not used here but may be helpful 
+    // ---------not used here but may be helpful in other applications ?relay drivers?
     float loc;
     loc = position / 100 * 125;
      if ( count - 125 >= (int)loc ) {
-        digitalWrite(port, LOW);
-        //LED_ON;
-    }
-    else {
-         digitalWrite(port, HIGH);
-        //LED_OFF;
-    }
+              digitalWrite(port, LOW);
+              //LED_ON; etc
+              }
+        else {
+              digitalWrite(port, HIGH);
+              //LED_OFF; etc 
+              }
 }
 
 
 
 void Init_INTERRUPTS(){
-    // this function initializes the interrupts
+    // this function initializes the interrupts 
      PCMSK |= (1 << PCINT0);                  // Pin Change Enable Mask for PB0 (pin 5) (datasheet page 52)
      sei();                                   // enable interrupts (MANDATORY)
 }
 
- #ifdef Send_10K_CAL   // for the calibration oscillator
+ #ifdef Send_10K_CAL   // for the calibration oscillator set a timer interrupt. 
         ISR (TIMER0_COMPA_vect) {
          PORTB ^= 1 << PINB3;        // Invert pin PB3
         }
  #endif 
-ISR(PCINT0_vect){
+ 
+ISR(PCINT0_vect){   // I have retained the original code and comments from Andreas's code.
          // interrupt service routine for pin change interrupt to measure the RC servo pulse 
          // modified to use TCNT0 to allow delay to work.. 
     // if PINB is HIGH, a rising edge interrupt has happened
@@ -177,18 +187,18 @@ ISR(PCINT0_vect){
    //   TCNT1 = 0;                           // reset counter (TCNT1 page 91 TCNT0 page 78
    //   TCCR1 = (1 << CS12) | (1 << CS11) | (1 << CS10);   // start timer1 with prescaler CK/64 --> 250 steps per 2ms (TCCR1 page 89
         TCNT0 = 0; 
-        TCCR0B = (1 <<CS01) | (1<< CS00) ; // start timero ck/64 page 80 (256 count is 2.048ms .. ) so 1.5ms (mid) should be 187. 1ms should be 127 or so. 
+        TCCR0B = (1 <<CS01) | (1<< CS00) ; // start timero ck/64 page 80 
+        // (if "perfect oscillator", then 256 counts is 2.048ms .. ) so 1.5ms (mid) would be 187. 1ms should be 127 or so.
+        // but if the pulse is > 2.048ms, it will "wrap" and possibly give errors if not caught  
         return;
        }
     // only reached when falling edge detected (end of servo pulse)
     //count = TCNT1;                           // take timer value to local variable
     //TCCR1 = 0;                               // stop timer
-      count = TCNT0;                           // take timer value to local variable ~ 125-254 if osc correct
-      TCCR0B = 0;                               // stop timer
+      count = TCNT0;                           // take timer value to local variable ~ aim for  125-254 for 1ms to 2ms 
+      TCCR0B = 0;                              // stop timer
       GIMSK &= ~(1 << PCIE);                   // Pin Change Interrupt Disable (datasheet page 51)
       pulse_ready=1;
-
-
 }
 
 
@@ -196,8 +206,8 @@ ISR(PCINT0_vect){
 void Stepper_Drive(int  in) {    // simple way to select 4 pin or 5 pin steppers etc. 
     if (HALF_STEP) {delayMicroseconds(StepSpeed);Stepper4_half_step_Drive(in);}
               else {delayMicroseconds(StepSpeed);Stepper4_Drive(in);}
-    
-   }
+  }
+
 void Stepper4_Drive ( int  in){
 //   Serial.print(in);
   /* stepper 4 phase changes
@@ -227,22 +237,15 @@ void Stepper4_Drive ( int  in){
 
 
 void Stepper4_HPDrive ( int  in){
-//   Serial.print(in);
+//   EXPERIMENT IN higher power drive.. rotating 45,135,225,315 instead of 0,90,180,270
+// Does not seem to give any higher torque in spite of higher current drain.
   /* stepper 4 phase changes
-   *  expects 0 - 3 input
- *  A+ A- B+ B- 
- *  1  0  0  0   <
- *  0  0  1  0   up
- *  0  1  0  0   >
- *  0  0  0  1   down
- * becomes
-   *  expects 0 - 3 input
  *  L(A+ A-) B+ B- 
- *  1  0  1  0   <up
- *  0  1  1  0   >upp
- *  0  1  0  1   >down 
- *  1  0  0  1   <down
- 
+ *  1  0  1  0   =45
+ *  0  1  1  0   =135
+ *  0  1  0  1   =225 
+ *  1  0  0  1   =315
+
  */
    uint8_t internal;
   internal= in % 4;    //(modulo keeps internal 0-3)
@@ -328,18 +331,16 @@ break;
 
 }
 
-
-
-
 void Achieved(){  
   PositionAchieved=true;digitalWrite(OUTA, LOW);digitalWrite(OUT_An, LOW);digitalWrite(OUTB, LOW);digitalWrite(OUT_Bn, LOW);
   }
 
 void Move_To (int pos){  
   int diff,aim,deadzone; bool dir;
-  aim=(pos*GAIN); // move this many steps per "unit" (nominal 0.1ms) of Rc pulse width .. (range 0-125) 
-  deadzone=GAIN*4;  // allow a deadzone for no response if only a small change is seen in the demand, to prevent "hunting" and allow drive to switch off
-  // how far from last position?  (half step is dealt with elsewhere) 
+  aim=(pos*GAIN);     // move this many steps per "unit" (circa 0.1ms/count) of Rc pulse width .. (range ~ 0-130, see iotest) 
+  deadzone=GAIN*1.5;  // allow a deadzone for no response if only a small change is seen in the demand, to prevent "hunting" and allow drive to switch off
+  
+  // how far from last position?  
   diff = aim - Stepper_Position; 
   if (abs(diff) >= (deadzone)){
     oldpos=Stepper_Position;
@@ -348,15 +349,14 @@ void Move_To (int pos){
                  // should probably be a while.. loop, but I had trouble with that and this works.. 
     if (dir) {for (int x=oldpos; x<=aim;x++){Stepper_Position=x;Stepper_Drive(Stepper_Position);}}
          else{for (int x=oldpos; x>=aim;x--){Stepper_Position=x;Stepper_Drive(Stepper_Position); }}
-    Achieved();           
+    Achieved();  // leaves current position in Stepper_Position         
     }
   }
 
 void Move_To_ABS (int pos){  
-  int diff,aim,deadzone; bool dir;
-  // No gains here! this code used only move absolute position mainly for range testing 
+  int diff,aim; bool dir;
+  // No gains here! this code used only move absolute position mainly for range testing, no deadzone either
   diff = pos - Stepper_Position;  
-//  if (abs(diff) >= (deadzone)){
     oldpos=Stepper_Position;
     dir=(abs(diff)== diff);
     if (HALF_STEP){aim=aim*2;}
@@ -364,7 +364,6 @@ void Move_To_ABS (int pos){
   if (dir) {for (int x=oldpos; x<=aim;x++){Stepper_Position=x;Stepper_Drive(Stepper_Position);}}
        else{for (int x=oldpos; x>=aim;x--){Stepper_Position=x;Stepper_Drive(Stepper_Position); }}
       Achieved();           
-  //   }
   }
 
 void Count_Motor_Range(){ // Move motor absolute 40 steps to explore range. 
@@ -378,24 +377,21 @@ void IOTEST(){ // measure the RC sevo signal
   int Hystresis;
   if ( pulse_ready) {
          pulse_ready = 0;
- if ((count>=100)&&(count<=255)){  //  this bit could do with adjustment to accomodate the deliberate osccal changes? 
-    if (Analog_mode){     
-        
-                   demand= int(count-100); //use 127*OSCNOM/100 ? Nominal servo range is approx 1ms to 2ms  (My digital tester gives 0.8ms to 2.2ms)
+         if ((count>=124)&&(count<=254)){  //  this bit could do with adjustment to accomodate the deliberate osccal changes? 124 to 256 for 1ms to 2ms
+             if (Analog_mode){     
+                   demand= int(count-124); //Nominal servo range is approx 1ms to 2ms  (My digital tester allows  0.8ms to 2.2ms)
                    PositionAchieved=false; }
- 
              else {  // end to end switched mode...add hystresis to avoid hunting Range Stepper_Position= 0 or Fullrange;?
                    Hystresis = 0; demand=0;
                    if (Stepper_Position>=50){Hystresis = 20;}
                    if (count>=(170-Hystresis)){demand=(2*FullRange)/GAIN;}  // drive to twice range 
                    PositionAchieved=false;
                           }
-                     } 
-
-                   
-         GIFR = (1 << PCIF);              // clear Pin Change Interrupt Flag  (datasheet page 52)
-         GIMSK |= (1 << PCIE); }           // Pin Change Interrupt Enable (datasheet page 51)
-        }
+          } 
+          GIFR = (1 << PCIF);   // clear Pin Change Interrupt Flag  (datasheet page 52)
+          GIMSK |= (1 << PCIE); // Pin Change Interrupt Enable (datasheet page 51)
+      }           
+  }
 
 void Int_driven_10k(){
     Calibrate_OSCILLATOR();
@@ -413,42 +409,44 @@ void Int_driven_10k(){
 }
 
 void Step_set_Zero(int Flashes){
-  
+// for identify at startup
 for (int x=1; x<=Flashes; x++){
       delay(50);
-      Stepper_Position=(2*FullRange)/Flashes;
+      Stepper_Position=(2*FullRange)/Flashes; //to hit endstop in flashes steps, aiming at 2x Fullrange ..;
       demand=0;
       PositionAchieved=false;
-      Move_To_ABS(0);  //to hit endstop, aiming at 2x Fullrange ..;
-    
-}}
+      Move_To_ABS(0);  
+      }
+}
 
 
 
 void setup(){
   #ifdef Send_10K_CAL 
-     Int_driven_10k();
+         Int_driven_10k();
   #else
+  
    Analog_mode= false;
+    
     #ifdef ANALOG
       Analog_mode= true;
     #endif
       
-    Calibrate_OSCILLATOR();
-    Init_PORT();
-    Init_INTERRUPTS();
+   Calibrate_OSCILLATOR();
+   Init_PORT();
+   Init_INTERRUPTS();
      if (Analog_mode){
         Step_set_Zero(4); // 4 flashes tells you its analog mode
         } 
         else{Step_set_Zero(1);
         }
    //calibration runs ?
-    #ifdef Count_range_steps
+   #ifdef Count_range_steps
          Count_Motor_Range();
-    #endif
+   #endif
     
   #endif
-  }
+ }
 
     
 void loop(){
